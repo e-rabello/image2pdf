2e48f368eff6fa691bbdcbea343c4c5912c8d922
e-rabello <8457978+e-rabello@users.noreply.github.com>
2024-03-25 16:22:49 -0300

    Update image2pdf.py

--- image2pdf.py
+++ image2pdf.py
@@ -1,9 +1,9 @@
 from fpdf import FPDF
 pdf = FPDF()
 # imagelist is the list with all image filenames
-imagelist = ['carta-1.png']
+imagelist = ['image1.png', 'image2.png', 'image3.png']
 
 for image in imagelist:
     pdf.add_page()
     pdf.image(image,0,0,210,297)
-pdf.output("cartaElvis.pdf", "F")
+pdf.output("file1.pdf", "F")
